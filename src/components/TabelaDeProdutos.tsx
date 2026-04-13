ff9d679633cd073a9de237e5a576779c370d6236
Carlos Ribeiro <carlosribeiro.orion@gmail.com>
2026-04-13 15:56:25 -0300

    Finalizando o componente TabelaDeProdutos.tsx

--- src/components/TabelaDeProdutos.tsx
+++ src/components/TabelaDeProdutos.tsx
@@ -1,12 +1,58 @@
+import dayjs from "dayjs";
 import type { Produto } from "../interfaces/Produto";
+import databaseDelete from '../assets/skin/database_delete.png';
 
 interface Props {
-    produtos: Produto[];
+  produtos: Produto[];
 }
 
-const TabelaDeProdutos = ({produtos}: Props) => {
+const TabelaDeProdutos = ({ produtos }: Props) => {
   return (
-    <div>{produtos[0].nome}</div>
-  )
-}
-export default TabelaDeProdutos
+    <div className="">
+      <table className="w-full border-2 border-gray-400">
+        <thead>
+          <tr className="border-2 border-gray-400 bg-gray-300">
+            <th className="border-r border-r-gray-200 p-1.5 font-semibold">Id</th>
+            <th className="border-r border-r-gray-200 p-1.5 font-semibold">Imagem</th>
+            <th className="border-r border-r-gray-200 p-1.5 font-semibold">Categoria</th>
+            <th className="border-r border-r-gray-200 p-1.5 font-semibold">Nome</th>
+            <th className="border-r border-r-gray-200 p-1.5 font-semibold">Disponível</th>
+            <th className="border-r border-r-gray-200 p-1.5 font-semibold">Data de Cadastro</th>
+            <th className="border-r border-r-gray-200 p-1.5 font-semibold">Preço</th>
+            <th className="border-r border-r-gray-200 p-1.5 font-semibold">Ação</th>
+          </tr>
+        </thead>
+        <tbody>
+          {produtos.map((produto, index) => (
+            <tr key={produto.id} className={"border border-gray-200 " + (index % 2 === 0 ? "bg-white" : "bg-gray-100")}>
+              <td className="border-r border-r-gray-200 text-center py-1 w-[8%]">{produto.id}</td>
+              <td className="border-r border-r-gray-200 text-center py-1 w-[10%]">
+                <div className="flex justify-center">
+                  <img src={produto.imagem} width="40px" />
+                </div>
+              </td>
+              <td className="border-r border-r-gray-200 text-center py-1 w-[13%]">{produto.categoria.nome}</td>
+              <td className="border-r border-r-gray-200 ps-2 py-1 w-[20%]">{produto.nome}</td>
+              <td className="border-r border-r-gray-200 text-center py-1 w-[13%]">{produto.disponivel ? "Sim" : "Não"}</td>
+              <td className="border-r border-r-gray-200 text-center py-1 w-[13%]">{dayjs(produto.dataCadastro).format("DD/MM/YYYY")}</td>
+              <td className="border-r border-r-gray-200 text-end pe-2 py-1 w-[10%]">{produto.preco.toLocaleString("pt-BR", {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+                useGrouping: true
+              })}</td>
+              <td className="border-r border-r-gray-200 text-center py-1 w-[13%]">
+                <button className="btn-danger px-4 py-1" type="button">
+                  <div className="flex items-center">
+                    <img className="me-1" src={databaseDelete} />
+                    Remover
+                  </div>
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+export default TabelaDeProdutos;
