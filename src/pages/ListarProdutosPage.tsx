6ec2c6d73af27803a81dfe5e7a89148ef607f41d
Carlos Ribeiro <carlosribeiro.orion@gmail.com>
2026-04-13 16:02:50 -0300

    Recuperando produtos do BD com fetch... then... then...

--- src/pages/ListarProdutosPage.tsx
+++ src/pages/ListarProdutosPage.tsx
@@ -1,8 +1,43 @@
+import { useEffect, useState } from "react";
 import TabelaDeProdutos from "../components/TabelaDeProdutos";
-import recuperarProdutos from "../util/recuperarProdutos";
 
 const ListarProdutosPage = () => {
-  const produtos = recuperarProdutos();
+  // const produtos = recuperarProdutos();
+
+  // const numeros: number[] = [1, 2, 3];
+  // console.log(numeros[0]);
+
+  // const [um, dois, tres]: number[] = [1, 2, 3];
+  // console.log(um);
+
+  const [produtos, setProdutos] = useState([]);
+  const [erro, setErro] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/produtos")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Ocorreu um erro ao recuperar produtos. Status: " + response.status);
+        }
+        return response.json();
+      })
+      .then((produtos) => {
+        setProdutos(produtos);
+      })
+      .catch ((error) => {
+        if (error instanceof Error) {
+          setErro(error.message)
+        }
+        else {
+          setErro("Ocorreu um erro desconhecido. Error: " + error);
+        }
+      })
+    }, [])
+
+  // "" em javascript é avaliado para falsy
+  if (erro) return <p className="text-lg">{erro}</p>;
+  if (produtos.length === 0) return <p className="text-lg">Recuperando produtos...</p>;
+
   return (
     <>
       <h1 className="text-xl font-semibold mb-1">Lista de Produtos</h1>
