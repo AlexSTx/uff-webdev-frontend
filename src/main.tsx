fbc39d2e996846d0739958e7d1d49364e037d0ef
Carlos Ribeiro <carlosribeiro.orion@gmail.com>
2026-04-15 21:08:45 -0300

    Inclusão da ordem de execução em ListarProdutosPage

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { RouterProvider } from 'react-router-dom'
+import './index.css'
 import router from './routes/router'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  // <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  // </StrictMode>,
 )
--- src/pages/ListarProdutosPage.tsx
+++ src/pages/ListarProdutosPage.tsx
@@ -3,7 +3,9 @@ import TabelaDeProdutos from "../components/TabelaDeProdutos";
 
 const ListarProdutosPage = () => {
   // const produtos = recuperarProdutos();
-
+  
+  console.log("1, 9");
+  
   // const numeros: number[] = [1, 2, 3];
   // console.log(numeros[0]);
 
@@ -12,16 +14,21 @@ const ListarProdutosPage = () => {
 
   const [produtos, setProdutos] = useState([]);
   const [erro, setErro] = useState("");
+  console.log("2, 10");
 
   useEffect(() => {
+    console.log("5");
+
     fetch("http://localhost:8080/produtos")
       .then((response) => {
         if (!response.ok) {
           throw new Error("Ocorreu um erro ao recuperar produtos. Status: " + response.status);
         }
+        console.log("7");
         return response.json();
       })
       .then((produtos) => {
+        console.log("8");
         setProdutos(produtos);
       })
       .catch ((error) => {
@@ -31,13 +38,19 @@ const ListarProdutosPage = () => {
         else {
           setErro("Ocorreu um erro desconhecido. Error: " + error);
         }
-      })
-    }, [])
+      });
+    console.log("6");  
+  }, [])
+
+  console.log("3, 11");
 
   // "" em javascript é avaliado para falsy
   if (erro) return <p className="text-lg">{erro}</p>;
-  if (produtos.length === 0) return <p className="text-lg">Recuperando produtos...</p>;
-
+  if (produtos.length === 0) {
+    console.log("4");
+    return <p className="text-lg">Recuperando produtos...</p>;
+  } 
+  console.log("12");
   return (
     <>
       <h1 className="text-xl font-semibold mb-1">Lista de Produtos</h1>
