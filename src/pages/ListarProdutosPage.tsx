663639adfcdfe71865208188842f8039c0d28775
Carlos Ribeiro <carlosribeiro.orion@gmail.com>
2026-04-15 21:20:13 -0300

    Projeto utilizando await/async

--- src/pages/ListarProdutosPage.tsx
+++ src/pages/ListarProdutosPage.tsx
@@ -2,47 +2,40 @@ import { useEffect, useState } from "react";
 import TabelaDeProdutos from "../components/TabelaDeProdutos";
 
 const ListarProdutosPage = () => {
-  // const produtos = recuperarProdutos();
-  
-  console.log("1, 9");
-  
-  // const numeros: number[] = [1, 2, 3];
-  // console.log(numeros[0]);
 
-  // const [um, dois, tres]: number[] = [1, 2, 3];
-  // console.log(um);
+  console.log("1, 10");
 
   const [produtos, setProdutos] = useState([]);
   const [erro, setErro] = useState("");
-  console.log("2, 10");
+
+  console.log("2, 11");
 
   useEffect(() => {
     console.log("5");
-
-    fetch("http://localhost:8080/produtos")
-      .then((response) => {
+    const getProdutos = async () => {
+      console.log("7");
+      try {
+        const response = await fetch("http://localhost:8080/produtos");
         if (!response.ok) {
           throw new Error("Ocorreu um erro ao recuperar produtos. Status: " + response.status);
         }
-        console.log("7");
-        return response.json();
-      })
-      .then((produtos) => {
-        console.log("8");
-        setProdutos(produtos);
-      })
-      .catch ((error) => {
-        if (error instanceof Error) {
-          setErro(error.message)
-        }
-        else {
-          setErro("Ocorreu um erro desconhecido. Error: " + error);
-        }
-      });
-    console.log("6");  
+        console.log("9");
+        setProdutos(await response.json());
+      } catch(error) {
+          if (error instanceof Error) {
+            setErro(error.message)
+          }
+          else {
+            setErro("Ocorreu um erro desconhecido. Error: " + error);
+          }
+      }
+    }
+    console.log("6");
+    getProdutos();
+    console.log("8");
   }, [])
 
-  console.log("3, 11");
+  console.log("3, 12");
 
   // "" em javascript é avaliado para falsy
   if (erro) return <p className="text-lg">{erro}</p>;
@@ -50,7 +43,7 @@ const ListarProdutosPage = () => {
     console.log("4");
     return <p className="text-lg">Recuperando produtos...</p>;
   } 
-  console.log("12");
+  console.log("13");
   return (
     <>
       <h1 className="text-xl font-semibold mb-1">Lista de Produtos</h1>
