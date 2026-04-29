83124fcf88bddfd49d8e305b4723b6dcc7083b80
Carlos Ribeiro <carlosribeiro.orion@gmail.com>
2026-04-29 09:52:32 -0300

    Paginação com dois botões concluída

--- src/hooks/useRecuperarProdutosComPaginacao.ts
+++ src/hooks/useRecuperarProdutosComPaginacao.ts
@@ -1,13 +1,13 @@
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import type { ResultadoPaginado } from "../interfaces/ResultadoPaginado";
 import type { Produto } from "../interfaces/Produto";
 
 const recuperarProdutosComPaginacao = async (pagina: number, tamanho: number): Promise<ResultadoPaginado<Produto>> => {
-  // const num = await new Promise<number>((resolve) => {
-  //   setTimeout(() => {
-  //       return resolve(1)
-  //   }, 1000)
-  // })  
+  const num = await new Promise<number>((resolve) => {
+    setTimeout(() => {
+        return resolve(1)
+    }, 1000)
+  })  
   // console.log(num);
   const response = await fetch(
     "http://localhost:8080/produtos/paginacao?pagina=" + pagina + "&tamanho="+ tamanho);
@@ -23,7 +23,7 @@ const useRecuperarProdutosComPaginacao = (pagina: number, tamanho: number) => {
   return useQuery({
     queryKey: ["produtos", "paginacao", pagina, tamanho],
     queryFn: () => recuperarProdutosComPaginacao(pagina, tamanho),
-    // staleTime: 10_000,
+    placeholderData: keepPreviousData
   });
 };
 export default useRecuperarProdutosComPaginacao;
--- src/pages/ProdutosComPaginacaoPage.tsx
+++ src/pages/ProdutosComPaginacaoPage.tsx
@@ -14,14 +14,16 @@ const ProdutosComPaginacaoPage = () => {
   if (errorRecuperarProdutos) throw errorRecuperarProdutos;
   if (recuperandoProdutos) return <p className="text-lg">Recuperando produtos...</p>;
 
-  const totalDePagina = resultadoPaginado.totalDePaginas;
+  const totalDePaginas = resultadoPaginado.totalDePaginas;
+  const produtos = resultadoPaginado.itens;
+
   return (
     <>
       <h1 className="text-xl font-semibold mb-1">Lista de Produtos</h1>
       <hr className="mb-4" />
       <TabelaDeProdutos produtos={produtos} />
       <button onClick={() => setPagina(pagina-1)} disabled={pagina === 0} className="btn-success px-4 py-1 me-3">Anterior</button> 
-      <button onClick={() => setPagina(pagina+1)} disabled={pagina === 1} className="btn-success px-4 py-1">Prómima</button> 
+      <button onClick={() => setPagina(pagina+1)} disabled={pagina === totalDePaginas - 1} className="btn-success px-4 py-1">Próxima</button> 
     </>
   )
 }
