22aa1a7edf9e265d5f6b748bc25903147ffb9437
Carlos Ribeiro <carlosribeiro.orion@gmail.com>
2026-04-29 10:51:17 -0300

    Implementação de barra de paginação

--- src/components/Paginacao.tsx
+++ src/components/Paginacao.tsx
@@ -1,11 +1,51 @@
 interface Props {
     pagina: number,
-    totalDePaginas: number
+    totalDePaginas: number,
+    tratarPaginacao: (pagina: number) => void
 }
 
-const Paginacao = ({pagina, totalDePaginas}: Props) => {
+const Paginacao = ({pagina, totalDePaginas, tratarPaginacao}: Props) => {
+
+  const pages = Array.from({length: totalDePaginas}).map((_, index) => index);  
+
   return (
-    <div>Paginacao</div>
+    <nav aria-label="Paginação">
+      <ul className="flex">
+        <li>
+          <button type="button" disabled={pagina === 0} onClick={() => tratarPaginacao(pagina-1)}
+            className={
+              "rounded-l-lg border border-gray-300 px-4 py-2 font-semibold hover:bg-gray-200 " +
+              (pagina === 0 ? "cursor-not-allowed bg-gray-300 opacity-50" : 
+                              "cursor-pointer bg-white text-green-700")
+            }
+          >
+            Anterior
+          </button>
+        </li>
+
+        {pages.map((page) => (
+          <li key={page}>
+            <button type="button" onClick={() => tratarPaginacao(page)} 
+              className={"cursor-pointer border px-4 py-2 font-semibold " + 
+                (pagina === page ? "border-green-800 bg-green-700 text-white" : 
+                                   "border-gray-300 bg-white text-green-600 hover:bg-gray-100")}
+            >
+              {page + 1}
+            </button>
+          </li>
+        ))}
+
+        <li>
+          <button type="button" disabled={pagina === totalDePaginas - 1} onClick={() => tratarPaginacao(pagina+1)}
+            className={"rounded-r-lg border border-gray-300 px-4 py-2 font-semibold hover:bg-gray-200 " + 
+              (pagina === totalDePaginas - 1 ? "cursor-not-allowed bg-gray-300 opacity-50" : 
+                                               "cursor-pointer bg-white text-green-700")}
+          >
+            Próxima
+          </button>
+        </li>
+      </ul>
+    </nav>
   )
 }
 export default Paginacao
--- src/pages/ProdutosComPaginacaoPage.tsx
+++ src/pages/ProdutosComPaginacaoPage.tsx
@@ -8,6 +8,10 @@ const ProdutosComPaginacaoPage = () => {
   const [pagina, setPagina] = useState(0);
   const tamanho = 3;
 
+  const tratarPaginacao = (pagina: number) => {
+    setPagina(pagina);
+  }
+
   const {data: resultadoPaginado,
         isPending: recuperandoProdutos,
         error: errorRecuperarProdutos} = useRecuperarProdutosComPaginacao({pagina: pagina.toString(), 
@@ -24,10 +28,10 @@ const ProdutosComPaginacaoPage = () => {
       <h1 className="text-xl font-semibold mb-1">Lista de Produtos</h1>
       <hr className="mb-4" />
       <TabelaDeProdutos produtos={produtos} />
-      {/* <Paginacao pagina={pagina} totalDePaginas={totalDePaginas} /> */}
+      <Paginacao pagina={pagina} totalDePaginas={totalDePaginas} tratarPaginacao={tratarPaginacao} />
 
-      <button onClick={() => setPagina(pagina-1)} disabled={pagina === 0} className="btn-success px-4 py-1 me-3">Anterior</button> 
-      <button onClick={() => setPagina(pagina+1)} disabled={pagina === totalDePaginas - 1} className="btn-success px-4 py-1">Próxima</button> 
+      {/* <button onClick={() => setPagina(pagina-1)} disabled={pagina === 0} className="btn-success px-4 py-1 me-3">Anterior</button> 
+      <button onClick={() => setPagina(pagina+1)} disabled={pagina === totalDePaginas - 1} className="btn-success px-4 py-1">Próxima</button>  */}
     </>
   )
 }
