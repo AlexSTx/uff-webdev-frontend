afdf9c6e8124dbbf81f107e99c5fd5fecfb3b0f7
Carlos Ribeiro <carlosribeiro.orion@gmail.com>
2026-04-29 21:37:11 -0300

    Adicionando isFetching em ProdutosComPaginacao.tsx

--- src/pages/ProdutosComPaginacaoPage.tsx
+++ src/pages/ProdutosComPaginacaoPage.tsx
@@ -4,35 +4,56 @@ import useRecuperarProdutosComPaginacao from "../hooks/useRecuperarProdutosComPa
 import Paginacao from "../components/Paginacao";
 
 const ProdutosComPaginacaoPage = () => {
-  
   const [pagina, setPagina] = useState(0);
   const tamanho = 3;
 
   const tratarPaginacao = (pagina: number) => {
     setPagina(pagina);
-  }
+  };
 
-  const {data: resultadoPaginado,
-        isPending: recuperandoProdutos,
-        error: errorRecuperarProdutos} = useRecuperarProdutosComPaginacao({pagina: pagina.toString(), 
-                                                                           tamanho: tamanho.toString()});
+  // isPending fica true quando não há dados ainda (primeira carga da query).
+  
+  // isFetching fica true sempre que há uma busca em andamento, inclusive na primeira carga 
+  // e nas refetches/trocas de página.
+
+  const {
+    data: resultadoPaginado,
+    isPending: recuperandoProdutos,
+    isFetching: atualizandoProdutos,
+    error: errorRecuperarProdutos,
+  } = useRecuperarProdutosComPaginacao({
+    pagina: pagina.toString(),
+    tamanho: tamanho.toString(),
+  });
 
   if (errorRecuperarProdutos) throw errorRecuperarProdutos;
-  if (recuperandoProdutos) return <p className="text-lg">Recuperando produtos...</p>;
+  if (recuperandoProdutos)
+    return <p className="text-lg">Recuperando produtos...</p>;
 
   const totalDePaginas = resultadoPaginado.totalDePaginas;
   const produtos = resultadoPaginado.itens;
 
   return (
     <>
-      <h1 className="text-xl font-semibold mb-1">Lista de Produtos</h1>
+      <h1 className="mb-1 text-xl font-semibold">Lista de Produtos</h1>
       <hr className="mb-4" />
       <TabelaDeProdutos produtos={produtos} />
-      <Paginacao pagina={pagina} totalDePaginas={totalDePaginas} tratarPaginacao={tratarPaginacao} />
+      <div className="flex">
+        <div className="flex flex-col items-center gap-2  ">
+          <Paginacao
+            pagina={pagina}
+            totalDePaginas={totalDePaginas}
+            tratarPaginacao={tratarPaginacao}
+          />
+          {atualizandoProdutos && (
+            <span className="text-sm text-green-700">Atualizando...</span>
+          )}
+        </div>
+      </div>
 
       {/* <button onClick={() => setPagina(pagina-1)} disabled={pagina === 0} className="btn-success px-4 py-1 me-3">Anterior</button> 
       <button onClick={() => setPagina(pagina+1)} disabled={pagina === totalDePaginas - 1} className="btn-success px-4 py-1">Próxima</button>  */}
     </>
-  )
-}
-export default ProdutosComPaginacaoPage
+  );
+};
+export default ProdutosComPaginacaoPage;
