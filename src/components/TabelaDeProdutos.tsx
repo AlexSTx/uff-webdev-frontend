937c1b5d3cd8c7c7745ae911f3292df5a0dc0835
Carlos Ribeiro <carlosribeiro.orion@gmail.com>
2026-05-04 13:12:57 -0300

    Projeto com implementação de remoção de produto

--- src/components/TabelaDeProdutos.tsx
+++ src/components/TabelaDeProdutos.tsx
@@ -5,9 +5,10 @@ import { Link } from "react-router-dom";
 
 interface Props {
   produtos: Produto[];
+  tratarRemocao: (id: number) => void;
 }
 
-const TabelaDeProdutos = ({ produtos }: Props) => {
+const TabelaDeProdutos = ({ produtos, tratarRemocao }: Props) => {
   return (
     <div className="overflow-x-auto mb-3">
       <table className="w-full border-2 border-gray-400">
@@ -44,7 +45,7 @@ const TabelaDeProdutos = ({ produtos }: Props) => {
                 useGrouping: true
               })}</td>
               <td className="border-r border-r-gray-200 text-center py-1 w-[13%]">
-                <button className="btn-danger px-4 py-1" type="button">
+                <button onClick={() => tratarRemocao(produto.id)} className="btn-danger px-4 py-1" type="button">
                   <div className="flex items-center">
                     <img className="me-1" src={databaseDelete} />
                     Remover
--- src/pages/ProdutosComPaginacaoPage.tsx
+++ src/pages/ProdutosComPaginacaoPage.tsx
@@ -3,6 +3,7 @@ import TabelaDeProdutos from "../components/TabelaDeProdutos";
 import useRecuperarProdutosComPaginacao from "../hooks/useRecuperarProdutosComPaginacao";
 import Paginacao from "../components/Paginacao";
 import Pesquisa from "../components/Pesquisa";
+import { useMutation } from "@tanstack/react-query";
 
 const ProdutosComPaginacaoPage = () => {
   const [pagina, setPagina] = useState(0);
@@ -17,6 +18,27 @@ const ProdutosComPaginacaoPage = () => {
     setNome(nome);
   };
 
+  const tratarRemocao = (id: number) => {
+    removerProduto(id);
+    setPagina(0);
+  }
+
+  const removerProdutoPorId = async (id: number) => {
+    const response = await fetch("http://localhost:8080/produto/" + id, {
+      method: "DELETE"
+    });
+    if (!response.ok) {
+      throw new Error ("Ocorreu um erro ao remover produto. Status code: " + response.status);
+    }
+    // return await response.json() - Não retorna nada uma vez que o back-end retorna void
+  }
+  const {
+    mutate: removerProduto,
+    error: errorRemoverProduto
+  } = useMutation({
+    mutationFn: (id: number) => removerProdutoPorId(id),
+
+  })
   // isPending fica true quando não há dados ainda (primeira carga da query).
   
   // isFetching fica true sempre que há uma busca em andamento, inclusive na primeira carga 
@@ -34,6 +56,7 @@ const ProdutosComPaginacaoPage = () => {
   });
 
   if (errorRecuperarProdutos) throw errorRecuperarProdutos;
+  if (errorRemoverProduto) throw errorRemoverProduto;
   if (recuperandoProdutos) return <p className="text-lg">Recuperando produtos...</p>;
 
   const totalDePaginas = resultadoPaginado.totalDePaginas;
@@ -45,7 +68,7 @@ const ProdutosComPaginacaoPage = () => {
       <hr className="mb-4" />
 
       <Pesquisa tratarPesquisa={tratarPesquisa} />
-      <TabelaDeProdutos produtos={produtos} />
+      <TabelaDeProdutos produtos={produtos} tratarRemocao={tratarRemocao} />
       <div className="flex">
         <div className="flex flex-col items-center gap-2  ">
           <Paginacao
@@ -58,9 +81,6 @@ const ProdutosComPaginacaoPage = () => {
           )}
         </div>
       </div>
-
-      {/* <button onClick={() => setPagina(pagina-1)} disabled={pagina === 0} className="btn-success px-4 py-1 me-3">Anterior</button> 
-      <button onClick={() => setPagina(pagina+1)} disabled={pagina === totalDePaginas - 1} className="btn-success px-4 py-1">Próxima</button>  */}
     </>
   );
 };
