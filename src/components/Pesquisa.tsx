4f52e6e80fe0ee74c345ed087dd7322b589e3df6
Carlos Ribeiro <carlosribeiro.orion@gmail.com>
2026-05-04 10:16:12 -0300

    Projeto com onChange para pesquisa

--- src/components/Pesquisa.tsx
+++ src/components/Pesquisa.tsx
@@ -1,18 +1,14 @@
-import { useRef } from "react"
 
 interface Props {
     tratarPesquisa: (nome: string) => void;
 }
 const Pesquisa = ({tratarPesquisa}: Props) => {
-  const nomeRef = useRef<HTMLInputElement>(null);  
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    tratarPesquisa(event.target.value);
+  }
+  
   return (
-    <form onSubmit={(event: React.SubmitEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        tratarPesquisa(nomeRef.current!.value);
-    }} className="flex mb-3">
-        <input ref={nomeRef} type="text" className="input me-3" />
-        <button type="submit" className="btn-success px-4 py-1">Pesquisar</button>
-    </form>
+    <input onChange={handleChange} type="text" className="input mb-3" placeholder="Informe o nome do produto desejado..." />
   )
 }
 export default Pesquisa
