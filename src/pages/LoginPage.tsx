8379f78a3488a0f06c8f812dbcaa92c6117238cd
Alexsander Teixeira <alexsandersoarest@gmail.com>
2026-06-26 00:35:48 -0300

    fix: remove unused import

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useLocation, useNavigate } from "react-router-dom";
 import z from "zod";
